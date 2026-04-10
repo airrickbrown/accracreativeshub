e60e9a28744c5be9a94df23a962f6cae79b7fd41
airrickbrown <airrickbrown@gmail.com>
2026-04-10 12:45:44 -0400

    fix password reset import path

--- accracreativeshub/src/main.tsx
+++ accracreativeshub/src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './AuthContext'
-import PasswordResetPage from './components/PasswordResetPage'
+import PasswordResetPage from './PasswordResetPage'
 
 function start() {
   const hash = window.location.hash
